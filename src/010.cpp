de9f5e22d32e3d5c967c1033ac62636d2b20803e
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2019-09-16 16:23:15 -0400

    Add descriptions

--- src/010.cpp
+++ src/010.cpp
@@ -1,3 +1,10 @@
+/*
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer
+n, and calls f after n milliseconds.
+*/
+
 #include <iostream>
 #include <windows.h>
 
